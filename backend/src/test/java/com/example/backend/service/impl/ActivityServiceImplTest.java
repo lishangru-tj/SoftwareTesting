bf4b0f2e7ff60f05a079791ee8979808cded2f76
sbw2053512 <83405314+sbw2053512@users.noreply.github.com>
2023-06-17 11:09:33 +0800

    Update ActivityServiceImplTest.java

--- backend/src/test/java/com/example/backend/service/impl/ActivityServiceImplTest.java
+++ backend/src/test/java/com/example/backend/service/impl/ActivityServiceImplTest.java
@@ -8,87 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 import static org.junit.Assert.*;
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = {BackEndApplication.class, ActivityServiceImpl.class})
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = {BackEndApplication.class, ActivityServiceImpl.class})
+//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+//@RunWith(SpringRunner.class)
+//@PropertySource(value = "classpath:application.properties")
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = BackEndApplication.class)
 public class ActivityServiceImplTest {
 
     @Autowired
     private ActivityServiceImpl activityServiceImpl;
-    @Test
-    public void getAll() {
-    }
-
-    @Test
-    public void getUserAllCreateActList() {
-    }
-
-    @Test
-    public void getUserAllApplyActList() {
-    }
-
-    @Test
-    public void getUserAllActList() {
-    }
-
-    @Test
-    public void getTestActList() {
-    }
-
-    @Test
-    public void getAct() {
-    }
 
     @Test
     public void createAct() {
+        //用例一
         Activity act=new Activity();
         act.setActivity_id(123L);
         Long ID=activityServiceImpl.CreateAct(act);
         System.out.println(ID);
-    }
-
-    @Test
-    public void deleteAct() {
-    }
-
-    @Test
-    public void updateAct() {
-    }
-
-    @Test
-    public void isCreator() {
-    }
-
-    @Test
-    public void setActivityState() {
-    }
-
-    @Test
-    public void getUserAllParticipateActList() {
-    }
-
-    @Test
-    public void getMapActList() {
-    }
-
-    @Test
-    public void patchActImages() {
-    }
-
-    @Test
-    public void getAvailableActs() {
-    }
-
-    @Test
-    public void activityHitNumPlus() {
-    }
-
-    @Test
-    public void modifyActivityRemind() {
+        //用例一
+        Activity act1=null;
+        //act.setActivity_id(123L);
+        Long ID1=activityServiceImpl.CreateAct(act1);
+        System.out.println(ID1);
     }
 }
